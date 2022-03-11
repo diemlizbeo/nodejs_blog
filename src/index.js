2ef9d7336f291a217eb893bceb2883729144a861
diemlizbeo <nguyendiemdiem2k1@gmail.com>
2022-03-11 17:07:30 +0700

    connect db

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ const express = require('express')
 const morgan = require('morgan')
 const handlebars = require('express-handlebars')
 
+
+const route = require('./routes')
+const db = require('./config/db')
+
+// Connect to DB
+db.connect()
+
 const app = express()
 const port = 3000
 
@@ -14,22 +21,10 @@ app.engine('hbs', handlebars({
   extname : '.hbs'
 }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'))
+app.set('views', path.join(__dirname, 'resources','views'))
 
-
-app.get('/', (req, res) => {
-  res.render('home')
-})
-
-
-app.get('/news', (req, res) => {
-  res.render('news')
-})
-
-app.get('/search', (req, res) => {
-  res.render('search')
-})
+route(app)
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
